f09c8b04bace5a9c597edd6df038d1dfeaef7098
lc <1259493451@qq.com>
2019-12-21 17:19:29 +0800

    完成

--- http/api.js
+++ http/api.js
@@ -1,5 +1,5 @@
 module.exports = { 
   getSession:'/api/mp/getSession',  // 获取code
   login:'/api/mp/login', // 登录路径
-  payWallet: '/api/mp/pay/payWallet'
+  payWallet: '/api/mp/pay/payWallet' // 充值接口地址，获取服务端返回的参数
 } 
--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,3 +1,4 @@
+//获取应用实例
 const app = getApp();
 const Api = app.Api;
 const Store = require('./../../utils/store.js')
@@ -9,12 +10,15 @@ Page({
   data: {
     index: 1
   },
+  // 获取文本框金额
   getMoney(e) {
     let amount = e.detail.value;
+    console.log(amount)
     this.setData({
       index: amount * 100
     })
   },
+  // 选择充值金额
   choose(e) {
     let data = e.currentTarget.dataset;
     this.setData({
@@ -22,39 +26,47 @@ Page({
     });
   },
   pay() {
-    app.get(Api.payWallet,{
-      openId: Store.getItem('openId'),
-      money: this.data.index
-    }).then((res) => {
-      // 支付
-      wx.requestPayment({
-        timeStamp: res.timeStamp,
-        nonceStr: res.nonceStr,
-        package: res.package,
-        signType: res.signType,
-        paySign: res.paySign,
-        success: function (errmsg) {
-          if (errmsg == 'requestPayment:ok') {
-            wx.showToast({
-              title: '支付成功',
-              icon: 'success'
-            });
+    let index = this.data.index
+    if (/^\d*$/.test(index) && index > 0){
+      app.get(Api.payWallet, {
+        openId: Store.getItem('openId'),
+        money: index
+      }).then((res) => {
+        // 支付
+        wx.requestPayment({
+          timeStamp: res.timeStamp,
+          nonceStr: res.nonceStr,
+          package: res.package,
+          signType: res.signType,
+          paySign: res.paySign,
+          success: function (errmsg) {
+            if (errmsg == 'requestPayment:ok') {
+              wx.showToast({
+                title: '支付成功',
+                icon: 'success'
+              });
+            }
+          },
+          fail: function (res) {
+            if (res.errMsg == 'requestPayment:fail cancel') {
+              wx.showToast({
+                title: '支付取消',
+                icon: 'none'
+              });
+            } else {
+              wx.showToast({
+                title: res.errmsg,
+                icon: 'none'
+              });
+            }
           }
-        },
-        fail: function (res) {
-          if (res.errMsg == 'requestPayment:fail cancel') {
-            wx.showToast({
-              title: '支付取消',
-              icon: 'none'
-            });
-          } else {
-            wx.showToast({
-              title: res.errmsg,
-              icon: 'none'
-            });
-          }
-        }
+        })
+      });
+    }else{
+      wx.showToast({
+        title: '请输入正确的金额',
+        icon:'none'
       })
-    });
+    }
   }
 })
